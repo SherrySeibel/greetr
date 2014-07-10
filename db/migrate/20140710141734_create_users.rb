5c3b68ca6df2689673532e4e06bbf2e022b75c85
Sherry Seibel <sherry.seibel@gmail.com>
2014-07-10 10:26:27 -0400

    Allow users to sign in using usernames

--- db/migrate/20140710141734_create_users.rb
+++ db/migrate/20140710141734_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username, null: false
-      t.string :password_digest, null: false 
+      t.string :password_digest, null: false
 
       t.timestamps null: false
     end
